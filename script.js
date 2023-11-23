bf72afecde453a0fbb2ae7c76bbec6aa0a3f76e6
Joviyal1 <122416120+Joviyal1@users.noreply.github.com>
2023-11-22 21:37:26 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    document.getElementById("student-info").innerText = "Student ID: 200536540  Name: Jovial Joy";
+});
+
+function submitOrder() {
+    const pizzaSize = document.getElementById("pizza-size").value;
+    const pizzaToppings = document.getElementById("pizza-toppings").value;
+    const pizzaCrust = document.getElementById("pizza-crust").value;
+
+    const pizza = new Pizza(pizzaSize, pizzaToppings, pizzaCrust);
+
+    document.getElementById("pizza-description").innerHTML = `<p>Your Pizza: ${pizza.getDescription()}</p>`;
+}
+
+class Pizza {
+    constructor(size, toppings, crust) {
+        this.size = size;
+        this.toppings = toppings;
+        this.crust = crust;
+    }
+
+    getDescription() {
+        return `Size: ${this.size} | Toppings: ${this.toppings} | Crust: ${this.crust}`;
+    }
+}
